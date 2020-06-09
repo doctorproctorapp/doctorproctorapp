73e13200f6f2bb99c232fbac0608410026c97b87
66579214 <andrewchung2004@gmail.com>
2020-06-08 19:02:59 -0700

    added graphql endpoint through appsync; need to add queries of data

---
+++ src/AppSync.tsx
@@ -0,0 +1,8 @@
+const awsmobile =  {
+    "aws_appsync_graphqlEndpoint": "https://bxe5v6iprzgmvcsh6qulm7cb4y.appsync-api.us-west-2.amazonaws.com/graphql",
+    "aws_appsync_region": "us-west-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-jp7u5wobkref3bwe3p5t5dv2jy",
+};
+
+export default awsmobile;
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,32 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.scss";
 // import * as serviceWorker from './serviceWorker';
-
+import Client from 'aws-appsync'
+import {ApolloProvider} from 'react-apollo'
+import {Rehydrated} from 'aws-appsync-react'
+import AppSync from './AppSync'
 import { BrowserRouter } from "react-router-dom";
 
+const client = new Client( {
+  url: AppSync.aws_appsync_graphqlEndpoint,
+  region: AppSync.aws_appsync_region,
+  auth: {
+    type: 'API_KEY',
+    apiKey: AppSync.aws_appsync_apiKey
+  }
+})
+
+const WithProvider = () => (
+  <ApolloProvider client = {client}>
+    <Rehydrated>
+      <App/>
+    </Rehydrated>
+  </ApolloProvider>
+)
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <WithProvider/>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
